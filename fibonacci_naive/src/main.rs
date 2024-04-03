8a2cda75de5093a0b6a6b5743a27a13bbd90bddf
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-04-02 21:01:09 -0600

    :sparkles: add matrix version of fibonacci + improve array

--- fibonacci_naive/src/main.rs
+++ fibonacci_naive/src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::time::Instant;
 
+const M: [[usize; 2]; 2] = [[1, 1], [1, 0]];
+
 fn main() {
     println!("Let's compute the value of the fibonacci sequence. Enter a number.");
 
@@ -30,6 +32,11 @@ fn main() {
     let array_computed_fibonacci = fibonacci_array(input);
     let elapsed = now.elapsed();
     println!("Fibonacci number {input} computed using bottom up array method is {array_computed_fibonacci}. Execution time: {elapsed:.2?}.");
+
+    let now = Instant::now();
+    let matrix_computed_fibonacci = fibonacci_matrix(input);
+    let elapsed = now.elapsed();
+    println!("Fibonacci number {input} computed using matrix method is {matrix_computed_fibonacci}. Execution time: {elapsed:.2?}.");
 }
 
 // basic recurssive definition
@@ -61,11 +68,52 @@ fn fibonacci_iterative(n: u32) -> u32 {
 
 // array-based (bottom up) definition
 fn fibonacci_array(n: usize) -> usize {
-    let mut fib: Vec<usize> = Vec::new();
-    fib.push(0);
-    fib.push(1);
-    for i in 2..n + 1 {
-        fib.push(fib.get(i - 1).unwrap().clone() + fib.get(i - 2).unwrap().clone())
+    match n {
+        0 => return 0,
+        1 => return 1,
+        _ => {}
     }
-    fib.get(n).unwrap().clone()
+
+    let mut fib: Vec<usize> = vec![0; n + 1];
+
+    fib[1] = 1;
+
+    for i in 2..=n {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    fib[n]
+}
+
+fn fibonacci_matrix(n: usize) -> usize {
+    let mut fib: [[usize; 2]; 2] = [[1, 1], [1, 0]];
+    if n != 0 {
+        power(&mut fib, n - 1);
+        fib[0][0]
+    } else {
+        n
+    }
+}
+
+fn power(fib: &mut [[usize; 2]; 2], n: usize) {
+    if n > 1 {
+        power(fib, n / 2);
+        multiply(fib, *fib);
+        if n % 2 != 0 {
+            multiply(fib, M);
+        }
+    } else {
+        return;
+    }
+}
+
+fn multiply(fib: &mut [[usize; 2]; 2], mib: [[usize; 2]; 2]) {
+    let x = fib[0][0] * mib[0][0] + fib[0][1] * mib[1][0];
+    let y = fib[0][0] * mib[0][1] + fib[0][1] * mib[1][1];
+    let z = fib[1][0] * mib[0][0] + fib[1][1] * mib[1][0];
+    let w = fib[1][0] * mib[0][1] + fib[1][1] * mib[1][1];
+    fib[0][0] = x;
+    fib[0][1] = y;
+    fib[1][0] = z;
+    fib[1][1] = w;
 }
