a9babd63607043a7a0d3a823149f73f878888e47
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-05-22 23:26:41 -0500

    :recycle: extract non-main functionality to lib

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,33 +16,8 @@ fn main() {
         config.query, config.file_path
     );
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() > 2 {
-            let query = args[1].clone();
-            let file_path = args[2].clone();
-
-            return Ok(Config { query, file_path });
-        }
-        return Err("Not enough arguments!");
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
 }
