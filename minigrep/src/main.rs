d390fb86885e401730145da99e0e624b82b7eecf
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-05-22 22:34:38 -0500

    :recycle: add configuration parse refactor

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,39 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for '{}' in file '{}'\n", query, file_path);
+    println!(
+        "Searching for '{}' in file '{}'\n",
+        config.query, config.file_path
+    );
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() > 2 {
+            let query = args[1].clone();
+            let file_path = args[2].clone();
+
+            return Ok(Config { query, file_path });
+        }
+        return Err("Not enough arguments!");
+    }
 }
